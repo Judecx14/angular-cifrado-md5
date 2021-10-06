7a37f444a2562fa4d3bdceb6e9440791af87f5f2
Cesar Hérnandez <cesarjaviersanchhdz@gmail.com>
2021-10-06 00:31:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Md5 } from 'ts-md5';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  formHash!: FormGroup;
+
   title = 'cifrado';
+
+  messageHash: any;
+
+  md5 = new Md5();
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.createForm();
+
+  }
+
+  ngDoCheck(): void {
+    // console.log("Sin hashear -->",this.formHash.get('message')?.value);
+    // console.log("Con hashear -->",this.md5.appendStr(this.formHash.get('message')?.value).end());
+    this.messageHash = this.md5.appendStr(this.formHash.get('message')?.value).end()
+  }
+
+  createForm(): void {
+    this.formHash = this.fb.group({
+      message: [''],
+      messageDecrpit: [{value: '', disabled: true}],
+    });
+  }
+
+
 }
